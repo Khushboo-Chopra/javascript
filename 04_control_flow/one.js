8fe3fffa309c848c035c7faa073ec2c76e7b6b6a
Khushboo Chopra <143963161+Khushboo-Chopra@users.noreply.github.com>
2023-12-23 17:20:28 +0000

    nullish coalescing operator

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -4,4 +4,12 @@ if(temp===41){
 }
 else{
     console.log(`temp is less than 50`);
-}
+} 
+
+//nullish coalescing operator (??) : null undefined
+let val1;
+val1 = 5 ?? 10;
+console.log(val1);
+
+//ternary operator
+//condition ? true : false
