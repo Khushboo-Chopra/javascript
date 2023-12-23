e1bf8d3081cd5fd16181710619259a10521535f5
Khushboo Chopra <143963161+Khushboo-Chopra@users.noreply.github.com>
2023-12-23 09:57:53 +0000

    merging different objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,50 @@
+//constructor method -> singleton object
+const tinderUser = new Object();
+//const tinderUser = {}; -> both are same
+tinderUser.id = "123abc";
+tinderUser.name = "Khushboo";
+tinderUser.isLoggedIn = false;
+console.log(tinderUser);
+
+const regularuser = {
+  email: "",
+  fullname: {
+    userFullName: {
+      firstName: "Khushboo",
+      lastName: "Chopra",
+    },
+  },
+};
+
+//console.log(regularuser.fullname.userFullName.firstName);
+
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"c",4:"d"};
+
+//merge to objects
+
+//const obj3 = {obj1,obj2};
+//const obj3 = Object.assign(obj1,obj2);
+//const obj3 = Object.assign({},obj1,obj2);
+
+//spread operator
+const obj3 = {...obj1,...obj2};
+
+console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"k@gmail.com"
+    },
+    {
+        id:2,
+        email:"s@gmail.com"
+    }
+];
+
+console.log(users[1].email);
+console.log(Object.keys(tinderUser)); // returns an array
+console.log(Object.values(tinderUser)); // returns an array
+console.log(Object.entries(tinderUser)); // returns an array
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
